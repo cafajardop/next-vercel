4dc8e9429d2594b06c043a690a6fc842663e6e48
cafajardop <carlosafp84@hotmail.com>
2022-07-05 22:18:37 -0500

    segunda carga

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { Navbar } from '../components/Navbar'
-import styles from '../styles/Home.module.css'
+import { Navbar } from '../../components/Navbar'
+import styles from '../../styles/Home.module.css'
 
-export default function AboutPage() {
+export default function ContactPage() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>About - Carlos</title>
-        <meta name="description" content="Home Page" />
+        <title>Contact - Carlos</title>
+        <meta name="description" content="Contact Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -16,15 +16,15 @@ export default function AboutPage() {
 
       <main className={styles.main}>
 
-         <h1>About Page</h1>
+         <h1>Contact Page</h1>
 
          <h1 className={styles.title}>          
-          Welcome <Link href="/" >About</Link>
+          Contact <Link href="/contact" >Contact</Link>
         </h1>
  
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/about.jsx</code>
+          <code className={styles.code}>pages/contact.jsx</code>
         </p>
       </main>      
     </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { Navbar } from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function HomePage() {
@@ -10,9 +12,12 @@ export default function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navbar/>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="/about">About</a>
+        <h1>Home Page</h1>
+        <h1 className={styles.title}>          
+          Ir a <Link href="/about" >About</Link>
         </h1>
  
         <p className={styles.description}>
