4fe50333bff5d04406979e4bb95ce47b6f3da0c8
GLOBANT\carlos.fajardo <carlos.fajardo@thetrevorproject.org>
2022-07-08 21:20:41 -0500

    Tercera carga

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,32 +1,18 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { Navbar } from '../components/Navbar'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import { MainLayout } from "../components/layouts/MainLayout";
 
 export default function AboutPage() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>About - Carlos</title>
-        <meta name="description" content="Home Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <MainLayout>
+      <h1>About Page</h1>
+      <h1 className="title">
+        Ir a <Link href="/">Home</Link>
+      </h1>
 
-      <Navbar/>
-
-      <main className={styles.main}>
-
-         <h1>About Page</h1>
-
-         <h1 className={styles.title}>          
-          Welcome <Link href="/" >About</Link>
-        </h1>
- 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/about.jsx</code>
-        </p>
-      </main>      
-    </div>
-  )
+      <p className="description">
+        Get started by editing 
+        <code className="code">pages/about.jsx</code>
+      </p>
+    </MainLayout>
+  );
 }
